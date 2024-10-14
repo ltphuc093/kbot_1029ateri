d1918ccdd5217f34aacb58d8ce7281596b36bbfc
Le Phuc <phuclt093@gmail.com>
2024-10-14 08:16:54 +0700

    test

---
+++ server.js
@@ -0,0 +1,24 @@
+// server.js
+const express = require('express');
+const { exec } = require('child_process');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Chạy file pip.js
+exec('node x.js', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
